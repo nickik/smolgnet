e8f4705da508cad9f310c960740fb6354f328f80
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-14 19:39:05 +0200

    Add GNet error model

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use core::fmt;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Error {
+    InvalidLength,
+    InvalidField,
+    InvalidCrc,
+    InvalidAddressForm,
+    InvalidSizeClass,
+    Unsupported,
+    NonCanonical,
+    BufferFull,
+    NoCredit,
+    LinkDown,
+    Desynchronized,
+    UnknownService,
+    UnknownTunnel,
+    UnknownStream,
+    InvalidState,
+    DirectionViolation,
+    ProfileViolation,
+    MessageTooLarge,
+    WouldBlock,
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for Error {}
