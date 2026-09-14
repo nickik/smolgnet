b3cdf33e9e5a045eda61682904a2d287c7fa2f8e
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-14 20:14:29 +0200

    Export new GNet endpoint, credit and discovery APIs

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,21 @@ pub mod gts;
 pub mod link;
 pub mod wire;
 
-pub use endpoint::{DirectLink, Endpoint, ListenerConfig, TunnelHandle};
+pub use endpoint::{
+    make_link_local, AddressAuthorityConfig, AddressState, DirectLink, Endpoint, EndpointConfig,
+    ListenerConfig, TunnelHandle, BOOTSTRAP_ADDRESS, LINK_LOCAL_PREFIX,
+};
 pub use error::{Error, Result};
-pub use gts::{Direction, GtsStream, GtsTunnel, TunnelRole, TunnelState};
-pub use link::{DlpEndpoint, Flit, Vcid};
+pub use gts::{
+    Direction, GtsStream, GtsTunnel, StreamState, TunnelRole, TunnelState,
+};
+pub use link::{DlpConfig, DlpEndpoint, Flit, LinkTraffic, VcMode, Vcid};
 pub use wire::css::{CssWire, ServiceSelector};
-pub use wire::gctl::{GctlMessage, GctlType};
-pub use wire::gdp::{AddressForm, GdpAddress, GdpHeader, GdpPacket, GdpType, GdpWireConfig, SizeClass};
+pub use wire::gctl::{
+    address_matches_prefix, normalize_prefix, AddressAck, AddressClaim, AddressNak, AddressOffer,
+    Advertise, CreditGrant, CreditRequest, DiscoveryScope, GctlMessage, GctlType, ServiceType,
+};
+pub use wire::gdp::{
+    AddressForm, GdpAddress, GdpHeader, GdpPacket, GdpType, GdpWireConfig, SizeClass,
+};
 pub use wire::gts::{GtsPacket, GtsType, StreamProfile};
